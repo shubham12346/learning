89b1e508a80bbdce512c762febf92d0f92a61523
shubham agrahari <aakashagrahari26@gmail.com>
2025-02-15 10:34:36 +0530

    Added day 8 for fetch auto retry

--- day8.js
+++ day8.js
@@ -1,3 +1,33 @@
-function fetchWithAutoRetry() {}
+// Impement a fetch autoretry functionwhich will atomatically retry
+// to fetch when errors occurs until the maximum retry limit is reached
 
-const fetchRetry = () => {};
+let counter = 1;
+const fetchWithAutoRetry = async (promiseFunction, maxLimit) => {
+  for (let index = 0; index < maxLimit; index++) {
+    try {
+      const res = await promiseFunction(3);
+      console.log(`Passed in ${index + 1} attempt  `, res);
+      return res;
+    } catch (err) {
+      console.log(`err ${index + 1} attempt failed `, err);
+    }
+  }
+  throw "All the attempts failed";
+};
+
+const fetchPromise = async (failLimit) => {
+  counter++;
+  return new Promise((resolve, reject) => {
+    if (counter > failLimit) {
+      setTimeout(() => {
+        resolve("promise resolved after 3 seconds");
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        reject("rejected ");
+      }, 1000);
+    }
+  });
+};
+
+fetchWithAutoRetry(fetchPromise, 6);
